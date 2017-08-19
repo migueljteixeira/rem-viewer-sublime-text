96fffd14fa63407ac05ab30e753cd6886d93ba22
Miguel Teixeira <mteixeira5@gmail.com>
2017-08-19 17:54:51 +0100

    We can redraw phantoms only on lines being modified.

--- src/label.py
+++ src/label.py
@@ -4,14 +4,16 @@ import sublime_plugin
 class Label:
     view = None
     region = None
+    offset = None
 
-    def __init__(self, view, region):
+    def __init__(self, view, region, offset):
         self.view = view
         self.region = region
+        self.offset = offset
 
     def get_row(self):
-        (row, col) = self.view.rowcol(self.region.a)
+        (row, col) = self.view.rowcol(self.offset + self.region.start())
         return row
 
     def get_value(self):
-        return self.view.substr(self.region).split("rem")[0]
+        return self.region.group()
--- src/viewer.py
+++ src/viewer.py
@@ -5,32 +5,17 @@ import sublime_plugin
 from .label import Label
 
 class Viewer:
-    REGEX = '([0-9]*[\.]*[0-9]+)rem(?=[;|\s])'
+    REGEX_PATTERN = r'([0-9]*[\.]*[0-9]+)rem(?=[;|\s])'
 
     view = None
+    regex = None
+    phantoms = {} # phantom per row
     phantom_set = []
 
     def __init__(self, view):
         self.view = view
-        self.phantom_set = self.__get_phantom_set()
-        self.draw()
-
-    def __get_phantom_set(self):
-        # Create a unique PhantomSet per view
-        return sublime.PhantomSet(self.view, str(self.view.id()))
-
-    def __get_labels(self, regions):
-        labels = {}
-        for region in regions:
-            label = Label(self.view, region)
-            row = label.get_row()
-
-            if row not in labels:
-                labels[row] = [label]
-            else:
-                labels[row].append(label)
-
-        return labels
+        self.regex = re.compile(self.REGEX_PATTERN)
+        self.phantom_set = sublime.PhantomSet(self.view, str(self.view.id()))
 
     def __get_html(self, values):
         return """
@@ -52,46 +37,62 @@ class Viewer:
 
         return sublime.Region(line.end())
 
-    def draw(self):
-        # Within this view, we look for all regions that match the given regex
-        regions = self.view.find_all(self.REGEX)
-        labels = self.__get_labels(regions)
-        phantoms = self.get_phantoms(labels)
-
-        self.phantom_set.update(phantoms)
-
-    def get_phantoms(self, labels_per_row):
-        phantoms = []
-
+    def __get_phantoms(self, labels_per_row):
         for row, labels in labels_per_row.items():
             values = []
             for label in labels:
                 values.append(label.get_value())
 
-            phantoms.append(
-                sublime.Phantom(self.__get_row_region(row), self.__get_html(values), sublime.LAYOUT_INLINE)
-            )
+            phanthom = sublime.Phantom(self.__get_row_region(row), self.__get_html(values), sublime.LAYOUT_INLINE)
 
-        return phantoms
+            self.phantoms[row] = phanthom
+
+        return list(self.phantoms.values())
+
+    def __get_labels(self, content, offset = 0):
+        labels = {}
+        found = False
+
+        for region in re.finditer(self.regex, content):
+            label = Label(self.view, region, offset)
+
+            row = label.get_row()
+            if row not in labels:
+                labels[row] = [label]
+            else:
+                labels[row].append(label)
+
+            found = True
+
+        if not found:
+            print('NOTHING!')
+            (row, col) = self.view.rowcol(offset)
+            labels[row] = []
+
+        return labels
+
+    def __draw(self, content, offset = 0):
+        labels = self.__get_labels(content, offset)
+        phantoms = self.__get_phantoms(labels)
+
+        self.phantom_set.update(phantoms)
 
     def enable(self):
         print('enable')
+        lines = sublime.Region(0, self.view.size())
+        content = self.view.substr(lines)
+
+        self.__draw(content)
 
     def disable(self):
         print('disable')
 
     def modify(self):
         print('modify')
-        line = self.view.line(self.view.sel()[0])
-        content = self.view.substr(line)
+        
+        for selection in self.view.sel():
+            line = self.view.line(selection)
+            content = self.view.substr(line)
+            offset = line.a
 
-        print(content)
-        # print(re.findall(r"([0-9]*[\.]*[0-9]+)rem(?=[;|\s])", content))
-
-        all_content = self.view.substr(sublime.Region(0, self.view.size()))
-
-        regex = re.compile(r"([0-9]*[\.]*[0-9]+)rem(?=[;|\s])")
-        for match in re.finditer(regex, all_content):
-            (row, col) = self.view.rowcol(match.start())
-            print(row)
-            # print(all_content[match.start() : match.end()])
+            self.__draw(content, offset)
